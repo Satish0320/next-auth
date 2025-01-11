e3c3da7ff66ef17b5c706da5043b21daf4ccdde3
satish0320 <rexmax2468@gmail.com>
2025-01-11 16:24:46 +0530

    credintials-provider

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,41 @@
 
 import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
 
-const handler = NextAuth({})
+const handler = NextAuth({
+    providers: [
+        CredentialsProvider({
+          name: "Email",
+          credentials: {
+            username: { label: "Username", type: "text", placeholder: "goku" },
+            password: { label: "Password", type: "password" }
+          },
+          async authorize(credentials) {
+            const username = credentials?.username
+            const password = credentials?.password
+
+            console.log(username);
+            console.log(password);
+            
+            const user = {
+              name: "satish",
+              id: "1",
+              username: "Goku"
+            }
+        
+            if (user) {
+              return user
+            }else{
+              return null
+            }
+          }
+        })
+      ]
+})
 
 export {handler as GET, handler as POST}
+
+
+
+
+
